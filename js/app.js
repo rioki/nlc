51200a6e4ab1efd98345836f2b15e632f4850b9f
Sean Farrell <sean.farrell@rioki.org>
2014-08-01 20:06:29 +0200

    Adds google analythics.

--- js/app.js
+++ js/app.js
@@ -576,3 +576,13 @@ App.UnlockController = Ember.Controller.extend({
     }
 });
 
+
+App.Router.reopen({
+  notifyGoogleAnalytics: function() {
+    return ga('send', 'pageview', {
+        'page': this.get('url'),
+        'title': this.get('url')
+      });
+  }.on('didTransition')
+});
+
